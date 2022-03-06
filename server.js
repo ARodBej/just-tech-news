e4ac386b9b8ef38ab0b24be4137c6cdd284a2992
Adilene Bejines <adi3511@hotmail.com>
2022-03-06 02:09:26 -0700

    added login/logout

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
 
-const app = express();
+const SequelizeStore = require('connect-session-sequelize')(session.Store);const app = express();
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  reserve: false, 
+  saveUninitialized: true,
+  store: new SequalizeStore({
+    db: sequalize
+  })
+};
+app.use(session(sess));
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
