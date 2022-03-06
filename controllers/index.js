15a7695d535bcdb7c14ca9b1d35632520bb5bd03
Adilene Bejines <adi3511@hotmail.com>
2022-03-06 04:53:04 -0700

    added delete-post function

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 // turn on routes
 app.use(routes);
